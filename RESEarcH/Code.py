b09474f8350f6cce6bf1d14ecff9a75a8854ba31
m-h0pe <133053207+m-h0pe@users.noreply.github.com>
2023-07-08 20:01:02 +0000

    for wood

---
+++ RESEarcH/Code.py
@@ -0,0 +1,33 @@
+# MAXIMUM SENSIBLE SOUND == 85 Db
+# Minimum sound you can hear== 0Db
+# SOUND < 45Db is not regarded as noise
+# THICKNESS OF MATERIALS
+# GLASS, WOOD AND CONCRETE
+# ENERGY OF LAWN MOWER=
+# HOW DISTANCE AND THICKNESS OF MATERIALS BLOCK NOISE E.g a wood must be at 25m with 2In thickness to block the noise of a lawnmower
+# Sound Absorption Coefficient depends on thickness 
+#  The Sound level decreases with increasing distance
+# DISTANCE RATIO
+# INVerse square law states that  the sound level decreases by 6Db for every doubling of the distance from the source
+import math
+
+# Parameters
+lawnmower_sound_level = 90  # dB
+distance_from_lawnmower = 20  # meters
+wooden_house_thickness = 12  # inches
+wood_absorption_coefficient = 0.05
+
+# Calculate sound level reduction due to distance
+distance_ratio = distance_from_lawnmower / 1  # Assuming lawnmower is at 0 distance
+sound_level_reduction = 6 * math.log2(distance_ratio)
+
+# Calculate additional sound level reduction due to absorption
+additional_sound_level_reduction = 10 * math.log10(1 - wood_absorption_coefficient)
+
+# Calculate total sound level reduction
+total_sound_level_reduction = sound_level_reduction + additional_sound_level_reduction
+
+# Calculate sound level at the wooden house
+sound_level_at_house = lawnmower_sound_level - total_sound_level_reduction
+
+print(f"The estimated sound level at the wooden house is approximately {sound_level_at_house:.2f} dB.")
